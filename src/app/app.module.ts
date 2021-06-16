0d276d7a823e88f606f57441f0901dc962331311
rahulyadav1986 <rahullooksforward@gmail.com>
2021-06-16 18:47:09 +0530

    Home Page done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import { BannerComponent } from './all-components/shared/banner/banner.component';
+import { FooterComponent } from './all-components/shared/footer/footer.component';
+import { HeaderComponent } from './all-components/shared/header/header.component';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HeaderComponent, BannerComponent, FooterComponent],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
