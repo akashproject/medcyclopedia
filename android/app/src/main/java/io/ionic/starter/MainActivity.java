ddc9d44d0e760519f48b4a9e60eca809b336dff2
projectscodeartssolution <projects@codeartssolution.com>
2021-08-01 10:42:52 +0530

    razor pay

--- android/app/src/main/java/io/ionic/starter/MainActivity.java
+++ android/app/src/main/java/io/ionic/starter/MainActivity.java
@@ -2,4 +2,22 @@ package io.ionic.starter;
 
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+import com.ionicframework.capacitor.Checkout;
+
+
+public class MainActivity extends BridgeActivity {
+
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+
+    // Initializes the Bridge
+    this.init(savedInstanceState, new ArrayList<Class<? extends Plugin>>() {{
+      // Additional plugins you've installed go here
+      // Ex: add(TotallyAwesomePlugin.class);
+      add(Checkout.class);
+    }});
+  }
+
+
+}
--- src/app/all-components/expert-counselling-content/expert-counselling-content.component.ts
+++ src/app/all-components/expert-counselling-content/expert-counselling-content.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
+import  'capacitor-razorpay';
+import { Plugins } from '@capacitor/core';
+const { Checkout } = Plugins;
+import {OrderServiceService} from 'src/app/all-services/order-service.service'
 @Component({
   selector: 'app-expert-counselling-content',
   templateUrl: './expert-counselling-content.component.html',
@@ -9,7 +13,14 @@ export class ExpertCounsellingContentComponent implements OnInit {
 
   date : string;
   ptiming : string;
-  constructor() { }
+
+  readioSelected:any;
+  readioSelected2:any;
+  readioSelected3:any;
+  startdate:any;
+  enddate:any;
+
+  constructor(private orderservice:OrderServiceService) { }
 
   ngOnInit() {}
 
@@ -18,19 +29,65 @@ export class ExpertCounsellingContentComponent implements OnInit {
   }
 
   getPreferredMode(event){
+    this.readioSelected2 = event
     console.log(event);
   }
 
   getPreferredLanguages(event){
+    this.readioSelected3 = event
     console.log(event)
   }
 
   
 
   registerNow(){
+    
 
     console.log(this.date);
     console.log(this.ptiming)
+
+    this.loadCheckout()
+  }
+
+  async loadCheckout() {
+    const options = { 
+      key: 'rzp_test_Ii0YAZ0ccSrcLs',
+      key_secret: 'uI4QCdMjvVTQQJ1LGMVauW8R', 
+      amount: '5000', 
+      
+      description: 'Credits towards consultation', 
+      image: 'https://i.imgur.com/3g7nmJC.png', 
+      currency: 'INR', 
+      name: 'Acme Corp', 
+      prefill: { 
+        email: 'gaurav.kumar@example.com', 
+        contact: '9999999999', 
+        name: 'Gaurav Kumar'
+      }, 
+      theme: {
+        color: '#3399cc'
+        }
+      }
+    try {
+    let data = (await Checkout.open(options));
+    console.log(data['response']['razorpay_payment_id'])
+
+    let date = this.date;
+    let time = this.ptiming;
+    let media = this.readioSelected2;
+    let order_id = data['response']['razorpay_payment_id'];
+    let language = this.readioSelected3;
+    let payment_id = data['response']['razorpay_payment_id'];
+    let payment_status = 'success';
+
+    this.orderservice.createOrder(date,time,media,order_id,language,payment_id,payment_status).subscribe(res=>{
+        console.log(res);
+      })
+
+
+    } catch (error) {
+      console.log(error['description'])
+    }
   }
 
 }
